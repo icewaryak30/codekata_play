52fcf1e89d63c7d0bf49ad730b0f63cc1ff1c20f
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-01-30 15:48:37 +0530

    Create even_factors.py

---
+++ even_factors.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=[]
+c=0
+for i in range(1,n+1):
+    if n%i==0:
+        l.append(i)
+for i in range(0,len(l)):
+    if l[i]%2==0:
+        c=c+1
+        if c==1:
+            print(l[i],end="")
+        else:
+            print("",l[i],end="")
