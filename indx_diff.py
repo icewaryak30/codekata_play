06ac4bfc7fca5109db31161203e31c0065135849
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-02-13 15:34:38 +0530

    Create indx_diff.py

---
+++ indx_diff.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=[int(i) for i in input().split()]
+x=max(l)
+y=min(l)
+for i in range(len(l)):
+	if l[i]==x:
+		i1=i
+		break
+for i in range(len(l)):
+	if l[i]==y:
+		i2=i
+		break
+print(i1-i2)
