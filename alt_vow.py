ab0c4921ba5c5b3abd2d861ab01735fd8675f968
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-03-13 14:43:22 +0530

    Update alt_vow.py

--- alt_vow.py
+++ alt_vow.py
@@ -2,11 +2,14 @@ vow=["a","e","i","o","u"]
 s=input()
 c=0
 x=[]
-for i in range(0,len(s)-1,2):
-	if s[i] in vow and s[i+1] not in vow or s[i] not in vow and s[i+1] in vow:
-		c+=2
-		x.append(c)
-	else:
-		c=0
-		x.append(c)
-print(max(x))
+if len(s)<2:
+	print(0)
+else:
+	for i in range(0,len(s)-1,2):
+		if s[i] in vow and s[i+1] not in vow or s[i] not in vow and s[i+1] in vow:
+			c+=2
+			x.append(c)
+		else:
+			c=0
+			x.append(c)
+	print(max(x))
